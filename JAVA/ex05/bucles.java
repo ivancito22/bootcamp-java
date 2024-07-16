eaef278c0207213a2c553878d2896ba04fcc4b59
ivancito22 <ivanbartrolomegmail.com>
2024-07-16 11:56:51 +0200

    Feat: JAVA bucles

---
+++ JAVA/ex05/bucles.java
@@ -0,0 +1,9 @@
+package ex05;
+
+public class bucles {
+        public static void main(String[] args) {
+          for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+          }  
+      }
+}
